5116333578a15a1f11cff062dc39b02f1dfb1440
HarshSinha7 <harshvijay577@gmail.com>
2025-12-25 19:15:38 +0530

    UPDATE: ADDED CLASSES AND MESSAGES LINK IN dashboard.html.

---
+++ src/main/java/com/project/controller/AdminController.java
@@ -0,0 +1,9 @@
+package com.project.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("admin")
+public class AdminController {
+}
