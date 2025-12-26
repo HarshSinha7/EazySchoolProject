bbc415b118dba105952376d01d5a8dd3610ca2a7
HarshSinha7 <harshvijay577@gmail.com>
2025-12-26 16:10:46 +0530

    UPDATE: -Added controller configuration for displayClass,addNewClass, deleteClass. -Added css for classes.html class.

--- src/main/java/com/project/controller/AdminController.java
+++ src/main/java/com/project/controller/AdminController.java
@@ -1,9 +1,59 @@
 package com.project.controller;
 
+import com.project.model.Classes;
+import com.project.model.Person;
+import com.project.repository.ClassesRepository;
+import com.project.repository.RegisterRepo;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("admin")
 public class AdminController {
+    @Autowired
+    ClassesRepository classesRepository;
+
+    @Autowired
+    RegisterRepo registerRepo;
+
+    @RequestMapping(value = "/displayClasses")
+    public ModelAndView classesController(){
+        List<Classes> classList=classesRepository.findAll();
+        ModelAndView view=new ModelAndView("classes.html");
+        view.addObject("classes", classList);
+        view.addObject("class", new Classes());
+        return view;
+    }
+
+    @PostMapping(value = "/addNewClass")
+    public ModelAndView addNewClass(@Valid @ModelAttribute("class") Classes classes, Errors error){
+        if(error.hasErrors()){
+            throw new RuntimeException("Class name contains errors");
+        }
+        classesRepository.save(classes);
+        ModelAndView view=new ModelAndView("redirect:/admin/displayClasses");
+        return view;
+    }
+
+    @RequestMapping(value = "/deleteClass")
+    public ModelAndView deleteClass(@PathVariable("id") int id){
+        Optional<Classes> optional=classesRepository.findById(id);
+        for(Person person:optional.get().getPersons()){
+            person.setClasses(null);
+            registerRepo.save(person);
+        }
+        classesRepository.deleteById(id);
+        ModelAndView view=new ModelAndView("redirect:/admin/displayClasses");
+        return view;
+    }
 }
---
+++ src/main/java/com/project/repository/ClassesRepository.java
@@ -0,0 +1,7 @@
+package com.project.repository;
+
+import com.project.model.Classes;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ClassesRepository extends JpaRepository<Classes, Integer> {
+}
